6ef4b96b9aff9080322affd6c7e326d96ea33fcc
Yash Borkar <imyashb00@gmail.com>
2025-01-14 18:22:46 +0530

    Create Verify_Preorder_Serialization_of_a_Binary_Tree.cpp

---
+++ 3_Walmart/Day 14/Verify_Preorder_Serialization_of_a_Binary_Tree.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/
+
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream ss(preorder);
+        string curr;
+        int nodes = 1;
+        while (getline(ss, curr, ',')) {
+            nodes--;
+            if (nodes < 0) return false;
+            if (curr != "#") nodes += 2;
+        }
+        return nodes == 0;
+    }
+};

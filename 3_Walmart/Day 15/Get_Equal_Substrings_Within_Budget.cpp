84a4ca8120386885fc77304128ceeafec7e7b1ae
Yash Borkar <imyashb00@gmail.com>
2025-01-15 18:04:57 +0530

    Create Get_Equal_Substrings_Within_Budget.cpp

---
+++ 3_Walmart/Day 15/Get_Equal_Substrings_Within_Budget.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/get-equal-substrings-within-budget/
+
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        int start = 0;
+        int currentCost = 0;
+        int maxLength = 0;
+
+        for (int end = 0; end < n; end++) {
+            currentCost += abs(s[end] - t[end]);
+
+            while (currentCost > maxCost) {
+                currentCost -= abs(s[start] - t[start]);
+                start++;
+            }
+
+            maxLength = max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+};

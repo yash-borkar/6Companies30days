f17aee616b3e344c288f3c5c67c36966b2cfe78e
Yash Borkar <imyashb00@gmail.com>
2025-01-22 14:25:05 +0530

    Create Kth_Largest_Element_in_a_Stream.cpp

---
+++ 5_Atlassian/Day 22/Kth_Largest_Element_in_a_Stream.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+    int num;
+    priority_queue<int, vector<int>, greater<int>> pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        num = k; 
+        for(auto ele : nums) {
+            pq.push(ele);
+            if(pq.size() > k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > num) pq.pop();
+        return pq.top();
+    }
+};
